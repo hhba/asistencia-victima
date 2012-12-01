93f258b9753cc08a93499afb3ec48059cfd1de92
Marcos Vanetta <marcos.vanetta@3pillarglobal.com>
2012-12-01 12:18:49 -0300

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AsistenciaVictima::Application.config.secret_token = '369fb0d6b30bac8535d00130d5b26e7616bf94aee60a18b3937e700eab6450fdbc39e5ac4de1adc92de33589dcd296dbea60ee2495b20b2894d78ba1d99e20bd'
