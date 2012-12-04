e807ff26bb1f2a4721304912641f827c5a29822b
Marcos Vanetta <marcos.vanetta@3pillarglobal.com>
2012-12-03 23:13:09 -0300

    fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -38,6 +38,7 @@ namespace :deploy do
   %w[start stop restart].each do |command|
     desc "#{command} unicorn server"
     task command, :roles => :app, :except => {:no_release => true} do
+      run "chmod a+x #{current_path}/config/unicorn_init.sh"
       run "/etc/init.d/unicorn_#{application} #{command}"
     end
   end
