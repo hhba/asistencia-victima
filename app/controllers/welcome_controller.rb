33f8af3d23c68880fe9288e689ec5b39a4eb7746
Marcos Vanetta <marcos.vanetta@3pillarglobal.com>
2012-12-26 19:16:49 -0300

    Remove rails app

--- app/controllers/welcome_controller.rb
+++
@@ -1,5 +0,0 @@
-class WelcomeController < ApplicationController
-  def index
-    @organizations = Organization.all
-  end
-end
--- config/unicorn.rb
+++
@@ -1,9 +0,0 @@
-root = "/home/malev/apps/asistencia-victima/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
-listen "/tmp/unicorn.asistencia-victima.sock"
-worker_processes 3
-timeout 30
