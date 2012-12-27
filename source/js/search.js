314e458877432460ebe4ddfd2e1bdb650ee00723
Marcos Vanetta <marcos.vanetta@3pillarglobal.com>
2012-12-27 19:32:45 -0300

    Drawing organizations on the map

--- source/js/search.js
+++ source/js/search.js
@@ -1,72 +1,11 @@
 var fustion_table_id = '1xxoIwan9TAwkh_AMkmfYQKbyUdQAvbR5WTvtgzw';
 var map;
+var mapper;
+var fusionProxy;
 var layerl0;
 var styles = [];
 var lastInfowindow = null;
 var defaultZoom = 5;
-
-ft2json.query(
-  'SELECT * FROM 1xxoIwan9TAwkh_AMkmfYQKbyUdQAvbR5WTvtgzw',
-  function(result) {
-    console.log(result);
-  }
-);
-
-var markers = {
-  getData: function(){
-   this.organizations = $("#marker-information").data("organizations");
-  },
-  drawOn: function(map){
-    this.template = $("#infowindowTemplate").html();
-    this.moreInfoTemplate = $("#moreInfoTemplate").html();
-    this.getData();
-    $.each(this.organizations, function(index, organization){
-      var address = organization.addresses[0];
-      if(address){
-        var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(address.latitude, address.longitude),
-          map: map,
-          animation: google.maps.Animation.DROP,
-          title: organization.name,
-          icon: organization.icon,
-          organization: organization
-        });
-        var infowindow = new google.maps.InfoWindow({
-          content: Mustache.render(markers.template, organization),
-          maxWidth: 300
-        });
-        google.maps.event.addListener(marker, 'click', function() {
-          if (lastInfowindow) lastInfowindow.close();
-          infowindow.open(map, marker);
-          lastInfowindow = infowindow;
-          $("#results").html(Mustache.render(markers.moreInfoTemplate, organization));
-        });
-      }
-    });
-  }
-};
-var geoLocalizator = {
-  successHandler: function(position){
-    newCenter = new google.maps.LatLng(position.coords.latitude, position.coords.longitude, {timeout: 5000});
-    map.setCenter(newCenter);
-    map.setZoom(15);
-    var marker = new google.maps.Marker({
-        position: newCenter,
-        map: map,
-        animation: google.maps.Animation.DROP,
-        title: "Tu ubicación actual"
-    });
-  },
-  errorHandler: function(msg){
-    // console.log(msg);
-  },
-  centerMap: function(map){
-    this.map = map;
-    if ("geolocation" in navigator) {
-      navigator.geolocation.getCurrentPosition(this.successHandler, this.errorHandler);
-    }
-  }
-};
 
 function initialize() {
   map = new google.maps.Map(document.getElementById('map_canvas'), {
@@ -173,9 +112,162 @@ searcher = {
   }
 };
 
+function FusionProxy(fusion_id){
+  this.fusion_id = fusion_id;
+  this.getData = function(callback){
+    var that = this;
+    ft2json.query('SELECT * FROM ' + this.fusion_id, function(results){
+      callback(that.formatter(results));
+    });
+  };
+  this.formatter = function(results){
+    this.data = _.map(results.data, function(result){
+      var lat = result.geo.split(", ")[0];
+      var lng = result.geo.split(", ")[1];
+      return {
+        name:    result["Organismo"],
+        address: result["Dirección postal"],
+        city:    result["Localidad"],
+        phone:   result["Teléfono"],
+        email:   result["email"],
+        web:     result["web"],
+        latLng: new google.maps.LatLng(lat, lng)
+      }
+    });
+    return this.data;
+  };
+}
+
+function Mapper(selector) {
+  this.selector = selector;
+  this.markers = [];
+  this.init = function(){
+    this.map = new google.maps.Map(document.getElementById(this.selector), {
+      center: new google.maps.LatLng(-33.65682940830173, -63.85107421875),
+      zoom: defaultZoom
+    });
+    return this.styleMap();
+  };
+  this.styleMap = function(){
+    var style = [
+      {
+        featureType: 'all',
+        elementType: 'all',
+        stylers: [
+          { saturation: -90 }
+        ]
+      }
+    ];
+    var styledMapType = new google.maps.StyledMapType(style, {
+      map: this.map,
+      name: 'Styled Map'
+    });
+    this.map.mapTypes.set('map-style', styledMapType);
+    this.map.setMapTypeId('map-style');
+    return this;
+  };
+  this.addMarkers = function(organizations){
+    var that = this;
+    this.markers = _.map(organizations, function(organization){
+      var marker = new google.maps.Marker({
+        position: organization.latLng,
+        map: that.map,
+        animation: google.maps.Animation.DROP,
+        title: organization.name,
+        //icon: organization.icon,
+        //organization: organization
+      });
+    });
+  };
+  this.addMarker = function(position, text){
+    var marker = new google.maps.Marker({
+        position: position,
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        title: text 
+    });
+    return marker;
+  };
+  this.centerMap = function(location, zoom) {
+    zoom = typeof zoom !== 'undefined' ? zoom : 15;
+    this.map.setCenter(location);
+    this.map.setZoom(zoom);
+    return this;
+  };
+}
+
+function GeoLocalizator() {
+  this.currentPosition = function(callback){
+    var that = this;
+    this.callback = callback;
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        function(position){
+          var newCenter = new google.maps.LatLng(position.coords.latitude, position.coords.longitude, {timeout: 5000});
+          that.callback(newCenter);
+        }, function(){
+          that.errorHandler();
+        }
+      );
+    }
+  };
+  this.errorHandler = function(msg){
+  };
+}
+
+function Marker(position){
+  this.position = position;
+}
+
+var Markers = {
+  getData: function(){
+   this.organizations = $("#marker-information").data("organizations");
+  },
+  drawOn: function(map){
+    this.template = $("#infowindowTemplate").html();
+    this.moreInfoTemplate = $("#moreInfoTemplate").html();
+    this.getData();
+    $.each(this.organizations, function(index, organization){
+      var address = organization.addresses[0];
+      if(address){
+        var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(address.latitude, address.longitude),
+          map: map,
+          animation: google.maps.Animation.DROP,
+          title: organization.name,
+          icon: organization.icon,
+          organization: organization
+        });
+        var infowindow = new google.maps.InfoWindow({
+          content: Mustache.render(markers.template, organization),
+          maxWidth: 300
+        });
+        google.maps.event.addListener(marker, 'click', function() {
+          if (lastInfowindow) lastInfowindow.close();
+          infowindow.open(map, marker);
+          lastInfowindow = infowindow;
+          $("#results").html(Mustache.render(markers.moreInfoTemplate, organization));
+        });
+      }
+    });
+  }
+};
+
 $(document).ready(function(){
-  initialize();
-  searcher.initialize(map);
+  mapper = new Mapper("map_canvas");
+  fusionProxy = new FusionProxy($("#fusion-information").data("fusion"));
+  geoLocalizator = new GeoLocalizator();
+  mapper.init();
+  fusionProxy.getData(function(results){
+    mapper.addMarkers(results);
+  });
+  geoLocalizator.currentPosition(function(position){
+    mapper.centerMap(position);
+    mapper.addMarker(position, "You are here");
+  });
+
+//  initialize();
+//  searcher.initialize(map);
   $("#btnSearch").click(function(event){
     event.preventDefault();
     searcher.exec($('#inputSearch').val());
